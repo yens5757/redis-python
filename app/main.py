74b43a5775c4c81b3f9705bc21ac0d48c2ca9e2a
Aiken <chikitlee2001@gmail.com>
2024-12-31 20:38:36 +1100

    testPongs2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, client_address = server_socket.accept() # wait for client
-    data = client_socket.recv(1024)
+    data = client_socket.recv(1024).decode('utf-8')
     split_data = data.split("PING")
     reply_string = ""
     for _ in len(split_data):
