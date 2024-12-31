f7ae68a9187c4e0635117360c6c104339693b283
Aiken <chikitlee2001@gmail.com>
2024-12-31 22:35:05 +1100

    testparser3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from app.redis_protocol_parser import RedisProtocolParser
+from redis_protocol_parser import RedisProtocolParser
 
 async def handle_client(reader, writer):
     while True:
