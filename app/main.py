074f45f9350f74e0e3653c7b45f3569698ae6b9b
Aiken <chikitlee2001@gmail.com>
2024-12-31 17:55:40 +1100

    testPong1

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
+    print("+PONG\r\n")
 
 
 if __name__ == "__main__":
